71b814ff20097875cf056d4fec297286149b388d
Hayden Johnson <hajohn100@gmail.com>
2025-04-08 17:03:34 -0700

    small fix with naming

--- hub/build_wg_config.sh
+++ hub/build_wg_config.sh
@@ -11,7 +11,6 @@ rm -f "$output_file"
 cp "base_config.txt" "$output_file"
 if [ -d "$directory" ]; then
     for file in ; do
-        filename=$(basename "$file")
-        cat "$filename" >> "$output_file"
+        cat "$file" >> "$output_file"
     done
 fi
