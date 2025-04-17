1a92c1cab8ebca5041607c30de2ea60b9bf76240
Galen St John <galen@eradeo.com>
2025-04-17 09:11:14 -0700

    reduce memory

--- spoke/startup.sh
+++ spoke/startup.sh
@@ -157,15 +157,15 @@ if [[ -n "$ENABLED_FOR_REQUESTS" ]]; then
         if [[ "$endpoint_config" == *"/v1/chat/completions"* ]]; then
             port=$(echo "$endpoint_config" | cut -d':' -f2 | cut -d'/' -f1)
             echo "Attempting to start vllm serve for completions on port $port"
-            completion_memory_needed_mib=18000
+            completion_memory_needed_mib=15000
             if [[ "$remaining_memory_mib" -gt "$completion_memory_needed_mib" ]]; then
                 completion_memory_fraction_float=$(awk "BEGIN{printf \"%.4f\", $completion_memory_needed_mib / $total_gpu_memory_mib}")
                 echo "Starting vllm serve for completions on port $port with fraction $completion_memory_fraction_float"
                 #mkdir -p /app/models
                 #cd /app/models
                 #test -f DeepSeek-R1-Distill-Qwen-14B-Q2_K_L.gguf || wget https://huggingface.co/unsloth/DeepSeek-R1-Distill-Qwen-14B-GGUF/resolve/main/DeepSeek-R1-Distill-Qwen-14B-Q2_K_L.gguf
-                # unsloth/DeepSeek-R1-Distill-Qwen-14B-bnb-4bit --enforce-eager --enable-reasoning --reasoning-parser deepseek_r1 --quantization bitsandbytes --load-format bitsandbytes --enable-chunked-prefill --max-num-seqs 1 /// this worked @ 16384, and according to https://huggingface.co/spaces/NyxKrage/LLM-Model-VRAM-Calculator using GGUF and Q4_K_M it should support 59392 context size with 17423.63 MiB used total
-                start_vllm_server "unsloth/DeepSeek-R1-Distill-Qwen-14B-bnb-4bit" generate "$port" "$completion_memory_fraction_float" "--tokenizer unsloth/DeepSeek-R1-Distill-Qwen-14B --load-format bitsandbytes --quantization bitsandbytes --enable-chunked-prefill --max_model_len 59392 --max-model-len 59392 --max-num-seqs 1 --enforce-eager --enable-reasoning --reasoning-parser deepseek_r1" # --enable-chunked-prefill oddly necessary to avoid OOM issues with this particular model serve setup
+                # unsloth/DeepSeek-R1-Distill-Qwen-14B-bnb-4bit --enforce-eager --enable-reasoning --reasoning-parser deepseek_r1 --quantization bitsandbytes --load-format bitsandbytes --enable-chunked-prefill --max-num-seqs 1 /// this worked @ 16384, and according to https://huggingface.co/spaces/NyxKrage/LLM-Model-VRAM-Calculator using GGUF and Q4_K_M it should support 43008 context size with 14.01GB used total
+                start_vllm_server "unsloth/DeepSeek-R1-Distill-Qwen-14B-bnb-4bit" generate "$port" "$completion_memory_fraction_float" "--tokenizer unsloth/DeepSeek-R1-Distill-Qwen-14B --load-format bitsandbytes --quantization bitsandbytes --enable-chunked-prefill --max_model_len 43008 --max-model-len 43008 --max-num-seqs 1 --enforce-eager --enable-reasoning --reasoning-parser deepseek_r1" # --enable-chunked-prefill oddly necessary to avoid OOM issues with this particular model serve setup
                 remaining_memory_mib=$((remaining_memory_mib - completion_memory_needed_mib))
                 started_completions=true
             else
